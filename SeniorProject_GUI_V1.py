48292c4f7b2b458cc09bf67095bd2635eb4f217f
barahonare <barahonare@hotmail.com>
2018-09-19 10:01:42 -0500

    Multiple function per button
    triggering multiple functions when a button is clicked and prompting new window

--- SeniorProject_GUI_V1.py
+++ SeniorProject_GUI_V1.py
@@ -2,6 +2,7 @@
 import math
 import tkinter as tk                # python 3
 from tkinter import font  as tkfont # python 3
+from time import sleep
 
 class RvmApp(tk.Tk):
 
@@ -19,7 +20,9 @@ class RvmApp(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (StartPage, RecyclePage, PurchasePage):
+        # for F in (StartPage, RecyclePage, PurchasePage):
+        pages = [StartPage, RecyclePage, RecycleScan]
+        for F in pages:
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -46,11 +49,11 @@ class StartPage(tk.Frame):
         label.pack(side="top", fill="x", pady=10)
 
         button1 = tk.Button(self, text="Recycle",
-                            command=lambda: controller.show_frame("RecycleScan"))
-        button2 = tk.Button(self, text="Purchase a Drink",
-                            command=lambda: controller.show_frame("PurchasePage"))
+                            command=lambda: controller.show_frame("RecyclePage"))
+        # button2 = tk.Button(self, text="Purchase a Drink",
+        #                     command=lambda: controller.show_frame("PurchasePage"))
         button1.pack()
-        button2.pack()
+        # button2.pack()
 
 
 class RecyclePage(tk.Frame):
@@ -64,7 +67,7 @@ class RecyclePage(tk.Frame):
        button1 = tk.Button(self, text="Plastic Bottle",
                            command=lambda: controller.show_frame(""))
        button2 = tk.Button(self, text="Aluminum Can",
-                           command=lambda: controller.show_frame(""))
+                           command=lambda: [controller.show_frame("RecycleScan"),Recycling()])
        button = tk.Button(self, text="Return to Start Page",
                           command=lambda: controller.show_frame("StartPage"))
        button1.pack()
@@ -75,12 +78,12 @@ class RecyclePage(tk.Frame):
 class RecycleScan(tk.Frame): #window after the recycle button is clicked
 
     def __init__(self, parent, controller):
-        tk.frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.label(self, bg="White", text="Please scan your recycleables with the scanner.", font=controller.title_font)
+        label = tk.Label(self, bg="White", text="Please scan your recycleables with the scanner.", font=controller.title_font)
         label.pack(fill="x")
-        #insert door opening command here
-        #after door closes
+        openDoor()
+        check_sensor()
         command=lambda: controller.show_frame("RecycleComplete")
 
 class RecycleComplete(tk.Frame):#window after the RecycleScan Window
@@ -88,43 +91,70 @@ class RecycleComplete(tk.Frame):#window after the RecycleScan Window
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.label(self, bg="White", text="Thank you for recycling.", font=controller.title_font)
+        label = tk.Label(self, bg="White", text="Thank you for recycling.", font=controller.title_font)
         label.pack(fill="x")
         #create timer code so the frame will wait 3 seconds
         command=lambda: controller.show_frame("StartPage")
 
 
-class PurchasePage(tk.Frame):
+# class PurchasePage(tk.Frame):
 
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, bg="lightgreen", text="What would you like to purchase?", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        PlasticBottleButton = tk.Button(self, text="Bottled Beverage",
-                            command=lambda: controller.show_frame(""))#need to add bottle servo dispensing code
-        CannedDrinkButton = tk.Button(self, text="Canned Beverage",
-                            command=lambda: controller.show_frame(""))#need to add canned servo dispensing code
-        PlasticBottleButton.pack()
-        CannedDrinkButton.pack()
-        counter = 0
-        def counter_label(label):
-            counter = 0
-            def count():
-                global counter
-                counter += 1
+#     def __init__(self, parent, controller):
+#         tk.Frame.__init__(self, parent)
+#         self.controller = controller
+#         label = tk.Label(self, bg="lightgreen", text="What would you like to purchase?", font=controller.title_font)
+#         label.pack(side="top", fill="x", pady=10)
+#         PlasticBottleButton = tk.Button(self, text="Bottled Beverage",
+#                             command=lambda: controller.show_frame(""))#need to add bottle servo dispensing code
+#         CannedDrinkButton = tk.Button(self, text="Canned Beverage",
+#                             command=lambda: controller.show_frame(""))#need to add canned servo dispensing code
+#         PlasticBottleButton.pack()
+#         CannedDrinkButton.pack()
+#         counter = 0
+#         def counter_label(label):
+#             counter = 0
+#             def count():
+#                 global counter
+#                 counter += 1
 
-        command=lambda: controller.show_frame("CheckoutWindow")
+#         command=lambda: controller.show_frame("CheckoutWindow")
 
-class CheckoutWindow(tk.Frame):#window after the PurchasePage Window
 
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, bg="White", text="Thank you for shopping at the RVM.", font=controller.title_font)
-        label.pack(fill="x")
-        #create timer code so the frame will wait 3 seconds
-        command=lambda: controller.show_frame("StartPage")		
+
+# class CheckoutWindow(tk.Frame):#window after the PurchasePage Window
+
+#     def __init__(self, parent, controller):
+#         tk.Frame.__init__(self, parent)
+#         self.controller = controller
+#         label = tk.Label(self, bg="White", text="Thank you for shopping at the RVM.", font=controller.title_font)
+#         label.pack(fill="x")
+#         #create timer code so the frame will wait 3 seconds
+#         command=lambda: controller.show_frame("StartPage")		
+
+# =============== Recycling methods ====================== #
+# ======================================================== #
+def Recycling():
+    openDoor()
+    check_sensor()
+    closeDoor()
+    
+
+
+def openDoor():
+    print("opening recycling door")
+
+def check_sensor():
+    print("Checking for objects")
+    print("Nothing is detected")
+    closeDoor()
+    
+def closeDoor():
+    print("Closing door")
+
+
+def dispense_drink(drink):
+    print("dispensing " + str(drink))
+
 
 if __name__ == "__main__":
 	app = RvmApp()
