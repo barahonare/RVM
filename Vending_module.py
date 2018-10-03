a8c9ca7d69a09c243c5ee328e0d884a88dc2ea18
barahonare <barahonare@hotmail.com>
2018-10-03 10:07:50 -0500

    Change GPIO
    change GPIO from 3 to 17

--- Vending_module.py
+++ Vending_module.py
@@ -4,7 +4,7 @@ from time import sleep
 # def main():
 #     servo1_motor()
 def servo1_motor():
-    pin_gpio = 3
+    pin_gpio = 17
     motor1 = gpiozero.Servo(pin_gpio)
     while True:
         motor1.min()
