216138c9574e00acf716a165bc06906d06287d1d
barahonare <barahonare@hotmail.com>
2018-10-14 11:45:08 -0500

    Not Running All Modules At Startup - Test4
    Previous methods fail. I decided to add the main module inside the Vending_module class

--- Vending_module.py
+++ Vending_module.py
@@ -1,8 +1,9 @@
 import gpiozero
 from time import sleep
 
-pin_gpio = 17
-servo1 = gpiozero.Servo(pin_gpio)
+def main():
+    pin_gpio = 17
+    main.servo1 = gpiozero.Servo(pin_gpio)
 
 # def main():
 #     servo1_motor()
@@ -15,11 +16,11 @@ servo1 = gpiozero.Servo(pin_gpio)
     #     motor1.mid()
     #     sleep(1)
 def servo1_open():
-    servo1.max()
+    main.servo1.max()
     
 def servo1_close():
-    servo1.min()
+    main.servo1.min()
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
