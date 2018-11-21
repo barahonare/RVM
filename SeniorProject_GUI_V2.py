41c6808f5c8f73425c555f981042172c4e3e3ce8
Anthony <asambilay95@gmail.com>
2018-11-20 19:53:55 -0600

    moved the metal detector into the selling module

--- SeniorProject_GUI_V2.py
+++ SeniorProject_GUI_V2.py
@@ -20,7 +20,7 @@ import tkinter as tk
 from Selling_Module import POS
 from Selling_Module import Stepper_Motor as STM
 from Selling_Module import CoinAcceptor as Coin
-import Metal_Detecter as MD
+from Selling_Module import Metal_Detecter as MD
 from tkinter import font as tkfont
 from tkinter import PhotoImage
 from tkinter import ttk
