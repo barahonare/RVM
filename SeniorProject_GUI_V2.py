00e01767ecddb459e4316722e8b8992c93856899
Anthony <asambilay95@gmail.com>
2018-11-24 14:25:05 -0600

    commed

--- SeniorProject_GUI_V2.py
+++ SeniorProject_GUI_V2.py
@@ -7,7 +7,7 @@ from Selling_Module import POS
 from Selling_Module import Stepper_Motor as STM
 from Selling_Module import CoinAcceptor as Coin
 from Selling_Module import Metal_Detecter as MD
-import PlasticDoorServo as PDS
+#import PlasticDoorServo as PDS
 from tkinter import font as tkfont
 from tkinter import PhotoImage
 from tkinter import ttk
