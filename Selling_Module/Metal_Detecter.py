5fbb5346ea871af3b5cdc1b19bbb275a27232dbd
barahonare <barahonare@hotmail.com>
2018-11-24 16:07:56 -0600

    change of servo library
    changed to RPi.GPIO library instead of using gpiozero

--- Selling_Module/Metal_Detecter.py
+++ Selling_Module/Metal_Detecter.py
@@ -1,30 +1,38 @@
 import serial
-from gpiozero import Servo
+# from gpiozero import Servo
+import RPi.GPIO as GPIO
 from time import sleep
 from Selling_Module import POS
 
 #Serial port and baudrate from Arduino
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-
-
 #Servo GPIO
-myGPIO=16
-servo = Servo(myGPIO)
+ServoPin = 36
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(ServoPin, GPIO.OUT)
+servo1 = GPIO.PWM(ServoPin, 50) # PWM with 50 Hz
+servo1.start(12.5)
+
 def main():
     pass
+
+def servo_max():
+    servo1.ChaneDutyCycle(12.5) # turn towards 180 degree
+def servo_min():
+    servo1.ChaneDutyCycle(2.5)
 
 def ScanToOpen(self,controller):
     #to change the label
     PurchasePage = controller.get_page('PurchaseMenu')
     while True:
-        servo.max()
+        servo_max()
         if b'METAL DETECTED\r\n' in ser:
             print('Metal Detected with Pi')
-            servo.min()
+            servo_min()
             print("min(unlock)")
             sleep(3)
-            servo.max()
+            servo_max()
             print("max(lock)")
             sleep(1)
             if POS.DiscountEnabler == 1:
@@ -34,3 +42,41 @@ def ScanToOpen(self,controller):
             break
 if __name__=="__Metal_Detecter__":
     main()
+
+
+# import serial
+# from gpiozero import Servo
+# from time import sleep
+# from Selling_Module import POS
+
+# #Serial port and baudrate from Arduino
+# ser = serial.Serial('/dev/ttyACM0', 9600)
+
+
+
+# #Servo GPIO
+# myGPIO=16
+# servo = Servo(myGPIO)
+# def main():
+#     pass
+
+# def ScanToOpen(self,controller):
+#     #to change the label
+#     PurchasePage = controller.get_page('PurchaseMenu')
+#     while True:
+#         servo.max()
+#         if b'METAL DETECTED\r\n' in ser:
+#             print('Metal Detected with Pi')
+#             servo.min()
+#             print("min(unlock)")
+#             sleep(3)
+#             servo.max()
+#             print("max(lock)")
+#             sleep(1)
+#             if POS.DiscountEnabler == 1:
+#                 POS.CanDiscountMethod(PurchasePage)
+#                 POS.DiscountReturnMethod(self)
+#             self.Scanninglabel.config(text = "Thank you for recycling!")
+#             break
+# if __name__=="__Metal_Detecter__":
+#     main()
