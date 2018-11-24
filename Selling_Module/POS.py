88ea188ef9a82cea61449f9d7a492cfa2c20c820
Anthony <asambilay95@gmail.com>
2018-11-24 13:34:12 -0600

    changed values on pos

--- Selling_Module/POS.py
+++ Selling_Module/POS.py
@@ -32,7 +32,7 @@ def CanDiscountMethod(self):
     global DiscountEnabler
     global FinalPrice
     if DiscountEnabler == 1:
-        Discount += .25
+        Discount += 25
         FinalPrice -= Discount
         self.TotalLabel.config(text = ("Your Final value is " '$%s      ' "Your discount was "'$%s'%(FinalPrice,Discount)))
         print("Discount is now set to $0.75")
@@ -42,7 +42,7 @@ def WaterDiscountMethod(self):
     global DiscountEnabler
     global FinalPrice
     if DiscountEnabler == 1:
-        Discount += .50
+        Discount += 50
         FinalPrice -= Discount
         self.TotalLabel.config(text = ("Your Final value is " '$%s      ' "Your discount was "'$%s'%(FinalPrice,Discount)))
         print("Discount is now set to $1.25")
@@ -73,7 +73,7 @@ def AddPriceOfSoda(self):
     global FinalPrice
     global Discount
     if SodaLimit < 1 and WaterLimit < 1:
-        Price += .75
+        Price += 75
         SodaLimit += 1
         FinalPrice = (FinalPrice + Price)
         #updates the label containing the total if the limit has not been met
@@ -89,7 +89,7 @@ def AddPriceOfWater(self):
     global FinalPrice
     global Discount
     if WaterLimit < 1 and SodaLimit < 1:
-        Price += 1.25
+        Price += 125
         WaterLimit += 1
         FinalPrice = (FinalPrice + Price)
         #updates the label containing the total if the limit has not been met
@@ -106,7 +106,7 @@ def SubtractPriceOfSoda(self):
     global Discount
     if SodaLimit > 0 and WaterLimit < 1:
         FinalPrice -= Price
-        Price -= .75
+        Price -= 75
         SodaLimit -= 1
         #updates the label containing the total if the limit has not been met
         self.Cartlabel.config(text = ("Your Cart Value is " '$%s' %Price))
@@ -122,7 +122,7 @@ def SubtractPriceOfWater(self):
     global Discount
     if WaterLimit > 0 and SodaLimit < 1:
         FinalPrice -= Price
-        Price -= 1.25
+        Price -= 125
         WaterLimit -= 1
         #updates the label containing the total if the limit has not been met
         self.Cartlabel.config(text = ("Your Cart Value is " '$%s' %Price))
@@ -136,11 +136,11 @@ def ResetPrice(self):
     global WaterLimit
     global FinalPrice
     global Discount
-    FinalPrice = 0.00
-    Discount = 0.00
+    FinalPrice = 000
+    Discount = 000
     SodaLimit = 0
     WaterLimit = 0
-    Price = 0.00
+    Price = 000
     self.Cartlabel.config(text = ('$%s' %Price))
     self.TotalLabel.config(text = ('$%s' %FinalPrice))
     print("Resetting price")
