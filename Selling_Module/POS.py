f497fab0a0da11cbb19423a380e18cd704bd07e4
Anthony <asambilay95@gmail.com>
2018-11-24 15:58:32 -0600

    took out test stuff in pos

--- Selling_Module/POS.py
+++ Selling_Module/POS.py
@@ -34,11 +34,7 @@ def CanDiscountMethod(self):
     global FinalPrice
     if DiscountEnabler == 1:
         Discount = 25
-        print("discount is this before math" ,Discount)
-        print("Final price is this before math" ,FinalPrice)
         FinalPrice -= Discount
-        print("discount is this after math" ,Discount)
-        print("Final price is this after math" ,FinalPrice)
         self.TotalLabel.config(text = ("Your Final value is " '$%s      ' "Your discount was "'$%s'%(FinalPrice,Discount)))
         print("Discount is now set to $0.25")
 #this method is to calculate the discount
