e0760ee4d6f953817e2621b6cf8fdd4c741d36d3
barahonare <barahonare@hotmail.com>
2018-11-26 14:50:39 -0600

    Stage3 Testing - change back pin
    return to board pin 33

--- Unit_Testing/Stage3/PlasticDoorServo.py
+++ Unit_Testing/Stage3/PlasticDoorServo.py
@@ -3,7 +3,7 @@ from time import sleep
 from Selling_Module import POS
 
 ### Servo GPIO
-ServoPin = 36
+ServoPin = 33
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(ServoPin, GPIO.OUT)
 servo1 = GPIO.PWM(ServoPin, 50) # PWM with 50 Hz
