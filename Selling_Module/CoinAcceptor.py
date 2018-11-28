1d03ef2e678e9eb740f6721be4e5dfeeedd6155e
anthony <43211456+asambilay@users.noreply.github.com>
2018-11-27 20:49:10 -0600

    Update CoinAcceptor.py
    upgrading the coin acceptor final code to the stage 5 code

--- Selling_Module/CoinAcceptor.py
+++ Selling_Module/CoinAcceptor.py
@@ -7,9 +7,8 @@ from Selling_Module import Stepper_Motor as SM
 def main():
     pass
 
-def ActivateCoinAcceptor(self, controller):
+def ActivateCoinAcceptor(self,controller):
     ser = serial.Serial('/dev/ttyACM1', 9600)
-    PurchaseMenuPage = controller.get_page("PurchaseMenu")
     while True:
         #Converting the byte to string
         s = ser.readline()
@@ -17,32 +16,32 @@ def ActivateCoinAcceptor(self, controller):
         sss = "".join(map(chr,ss))
         #Convert the string to integers
         total_amount = int(sss)
-        self.Final_amount = POS.Price
+        self.Final_amount = POS.FinalPrice
         print(total_amount)
         if total_amount >= 25:
             self.coinlabeltest.config(text = "You have deposited $0.25")
             self.Final_amount -= 25
-            self.FinalTotalLabel.config(text = '$%s' %self.Final_amount)
+            self.FinalTotalLabel.config(text = '$%.2f' %(self.Final_amount/100))
             self.update()
         if total_amount >= 50:
             self.coinlabeltest.config(text = "You have deposited $0.50")
             self.Final_amount -= 25
-            self.FinalTotalLabel.config(text = '$%s' %self.Final_amount)
+            self.FinalTotalLabel.config(text = '$%.2f' %(self.Final_amount/100))
             self.update()
         if total_amount >= 75:
             self.coinlabeltest.config(text = "You have deposited $0.75")
             self.Final_amount -= 25
-            self.FinalTotalLabel.config(text = '$%s' %self.Final_amount)
+            self.FinalTotalLabel.config(text = '$%.2f' %(self.Final_amount/100))
             self.update()
         if total_amount >= 100:
             self.coinlabeltest.config(text = "You have deposited $1.00")
             self.Final_amount -= 25
-            self.FinalTotalLabel.config(text = '$%s' %self.Final_amount)
+            self.FinalTotalLabel.config(text = '$%.2f' %(self.Final_amount/100))
             self.update()
         if total_amount >= 125:
             self.coinlabeltest.config(text = "You have deposited $1.25")
             self.Final_amount -= 25
-            self.FinalTotalLabel.config(text = '$%s' %self.Final_amount)
+            self.FinalTotalLabel.config(text = '$%.2f' %(self.Final_amount/100))
             self.update()
         if total_amount >= POS.FinalPrice:
             self.coinlabeltest.config(text = "Thank you for choosing RVM")
@@ -52,7 +51,6 @@ def ActivateCoinAcceptor(self, controller):
                 SM.Stepper2Forward(self)
             print(total_amount, POS.Price)
             ser.close()
-            POS.ResetPrice(PurchaseMenuPage)
             controller.show_frame("MainMenu")
             break
 if __name__=="__CoinAcceptor__":
